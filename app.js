3829e0a16ce6912201e994dcb5a9fecc334a3046
Alex Gardell <agardell89@gmail.com>
2018-12-02 11:14:24 -0500

    connect to postgres DB

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 const express = require("express");
 const app = express();
-const PORT = "5500";
+const { Pool } = require("pg");
+const PORT = process.env.PORT;
+
+// require my ENV file to set envrionment variables
+require("dotenv").config();
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: "localhost",
+    database: "gardellwagnerwedding",
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+  });
+
+pool.connect(err => {
+    if (err) {
+        console.log("Error: " + err);
+    } else {
+        pool.query("SELECT * FROM guestsconfirmed", (err, res) => {
+            if (!err){
+                console.log(res.rows[0]);
+            } else {
+                console.log (err);
+            }
+            pool.end();
+        });
+    }
+});
 
 // set view engine to ejs
 app.set("view engine", "ejs");
@@ -11,5 +38,5 @@ var mainRoute = require("./routes/index.js");
 app.use(mainRoute);
 
 app.listen(PORT, () => {
-    console.log("Website started on port 5500!");
+  console.log("Website started on port 5500!");
 });
